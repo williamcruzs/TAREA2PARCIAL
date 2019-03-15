5d87830cdae44d3275322caf79704cfe6002b4b3
Cesia Zamora <Cesia Zamora@192.168.0.6>
2019-03-14 23:11:41 -0600

    commit

---
+++ src/tareaabstraccion/Universidad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaabstraccion;
+
+/**
+ *
+ * @author Cesia Zamora
+ */
+public abstract class Universidad {
+    
+
+//Metodos Abstractos
+    abstract String getUniversidad();
+    abstract String getPresidente();
+    
+          
+}
