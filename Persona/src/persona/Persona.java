538d27bede4c6531ed6021eac77dd9d774457014
williamcruzs <48373389+williamcruzs@users.noreply.github.com>
2019-03-15 19:07:11 -0500

    tercer commint

--- Persona/src/persona/Persona.java
+++ Persona/src/persona/Persona.java
@@ -9,13 +9,40 @@ package persona;
  *
  * @author HP 9480m
  */
-public class Persona {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public abstract class Persona {
     
-}
+    
+    
+    //abstraccion;
+    abstract String getLocalidad();
+    
+    //Encapsulamiento
+    public String nombre;
+     public String pasantia;
+    private int edad;
+    
+    public void setNombre(String _nombre){
+        this.nombre =_nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setPasantia(String _pasantia){
+        this.pasantia =_pasantia;
+    }
+    public String getPasantia(){
+        return pasantia;
+    }
+    public void setEdad(int _edad){
+        this.edad =_edad;
+    }
+    public int getEdad(){
+        return edad;}
+    
+        public void Imprimirdatos(){
+       System.out.println("Nombre" + nombre);
+        System.out.println("Pasantia" + pasantia);
+    
+        }
+     //poliformismo
+     abstract String obtenerInformacionpersona();}
