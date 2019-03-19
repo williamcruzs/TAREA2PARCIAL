d7e2ff4d0732550a8d25ceddafcd0f61d5c9a268
Cesia Zamora <Cesia Zamora@192.168.0.5>
2019-03-18 23:10:30 -0600

    Modificar subclase estudiante

--- TAREA2PARCIAL/src/tarea2parcial/Estudiante.java
+++ TAREA2PARCIAL/src/tarea2parcial/Estudiante.java
@@ -39,7 +39,7 @@ public String getUniversidad(){
     
     @Override
     String getLocalidad() {
-        return " "; //To change body of generated methods, choose Tools | Templates.
+        return " El Progreso"; //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
