4afe445b66523fa43b2b7e714f8e94d83c0ae2ab
Cesia Zamora <Cesia Zamora@192.168.0.6>
2019-03-19 23:29:23 -0600

    Subclase Estudiante

---
+++ Es/src/Estudiante.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2parcial;
+
+/**
+ *
+ * @author Cesia Zamora
+ */
+public class Estudiante extends Personas{
+public String carrera;
+public String universidad;
+
+public Estudiante(){
+    setNombre("Cesia");
+    setEdad(21);
+    setSexo("Femenino");
+    setPasantia("Estudiar");
+    setCarrera("Ing. Poducción Industrial");
+    setUniversidad("UTH");
+}
+
+ public void setCarrera(String _carrera){
+    this.carrera = _carrera;
+}
+
+public String getCarrera(){
+    return carrera;
+}
+
+public void setUniversidad(String _universidad){
+    this.universidad = _universidad;
+}
+
+public String getUniversidad(){
+    return universidad;
+}
+    
+    @Override
+    String getLocalidad() {
+        return "El Progreso"; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    String obtenerInformacionpersona() {
+       super.Imprimirdatos();
+       return  "Carrera: "+getCarrera()+ "\n" +"Universidad: "+ getUniversidad(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
